439e63baa84dce161fd12b3fbb54c948f17f3dba
AZ1M1-007 <mustafaazimi0404@gmail.com>
2026-04-06 15:00:59 +0430

    chapter 11 - 20

---
+++ book questions/chapter5/q15.js
@@ -0,0 +1,22 @@
+// (Display the ASCII character table) Write a program that prints the characters in 
+// the ASCII character table from ! to ~. Display ten characters per line. The ASCII 
+// table is shown in Appendix B. Characters are separated by exactly one space.
+
+let line = "";
+let count = 0;
+
+for (let i = 33; i <= 126; i++) {
+    line += String.fromCharCode(i) + " ";
+    count++;
+
+    if (count === 10) {
+        console.log(line);
+        line = "";
+        count = 0;
+    }
+}
+
+
+if (line !== "") {
+    console.log(line);
+}
